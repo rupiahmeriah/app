99da20ba536fd6faf5c49cca5428ae9262297fb9
Egan Bisma <brickkace@gmail.com>
2023-01-31 11:41:09 +0700

    categories table UI

--- pages/setup/categories/index.tsx
+++ pages/setup/categories/index.tsx
@@ -1,5 +1,6 @@
 import { Session } from "@supabase/auth-helpers-react";
 import { useSessionContext } from "@supabase/auth-helpers-react";
+import CategoriesTable from "../../../modules/setup/categories/components/CategoriesTable";
 import { TabsLayout } from "../TabsLayout";
 
 export default function Example({
@@ -18,11 +19,9 @@ export default function Example({
       <div className="min-h-full">
         <div className="flex flex-1 flex-col lg:pl-10">
           <main className="flex-1 pb-8">
-            {/* Page header */}
-            <div className="px-4 sm:px-6 lg:max-w-6xl">
+            <div className="px-4 sm:px-6">
               <div className="py-6 md:flex md:items-center md:justify-between">
                 <div className="min-w-0 flex-1">
-                  {/* Profile */}
                   <div className="flex items-center">
                     <div>
                       <div className="flex items-center">
@@ -35,7 +34,7 @@ export default function Example({
                 </div>
               </div>
 
-              <div className="mt-2 grid grid-cols-[minmax(200px,auto)_1fr_0.3fr] grid-flow-row">
+              <div className="mt-2 ">
                 <div>
                   <button
                     type="button"
@@ -44,6 +43,8 @@ export default function Example({
                     Add New Category
                   </button>
                 </div>
+
+                <CategoriesTable />
               </div>
             </div>
           </main>
